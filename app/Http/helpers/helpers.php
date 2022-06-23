d7e207b48b6cd98f9b5b10e17407236863a15c80
ekaf08 <105408310+ekaf08@users.noreply.github.com>
2022-06-23 08:39:17 +0700

    add helper-tanggal_indonesia

--- app/Http/helpers/helpers.php
+++ app/Http/helpers/helpers.php
@@ -8,7 +8,7 @@ function format_uang($angka)
 function terbilang($angka)
 {
     $angka = abs($angka);
-    $baca = array('', 'satu ', 'dua ', 'tiga ', 'empat ', 'lima ', 'enam ', 'tujuh ', 'delapan ', 'sembilan ', 'sepuluh ', 'sebelas ');
+    $baca = array('', 'Satu ', 'Dua ', 'Tiga ', 'Empat ', 'Lima ', 'Enam ', 'Tujuh ', 'Delapan ', 'Sembilan ', 'Sepuluh ', 'Sebelas ');
     $terbilang = '';
 
     if ($angka < 12) {
@@ -30,4 +30,23 @@ function terbilang($angka)
     }
 
     return $terbilang;
+}
+
+function tanggal_indonesia($tgl, $tampil_hari = true)
+{
+    $nama_hari = array('Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', "Jum'at", 'Sabtu');
+    $nama_bulan = array(1 => 'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember');
+
+    $tahun = substr($tgl, 0, 4);
+    $bulan = $nama_bulan[(int) substr($tgl, 5, 2)];
+    $tanggal = substr($tgl, 8, 2);
+    $text = '';
+    // 2022-06-23
+    if ($tampil_hari) {
+        $urutan_hari = date('w', mktime(0, 0, 0, substr($tgl, 5, 2), $tanggal, $tahun));
+        $hari = $nama_hari[$urutan_hari];
+        $text .= $hari . ", " . $tanggal . " " . $bulan . " " . $tahun;
+
+        return $text;
+    }
 }
