877630efec77272a5e6283193bfeeb850b04dd3d
ekaf08 <105408310+ekaf08@users.noreply.github.com>
2022-07-12 10:30:46 +0700

    error cetak.blade

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -129,16 +129,20 @@ class MemberController extends Controller
 
     public function cetakMember(Request $request)
     {
-        $datamember = array();
+        $datamember = collect(array());
         foreach ($request->id as $id) {
             $member = Member::find($id);
             $datamember[] = $member;
         }
 
-        return $datamember;
+        $datamember = $datamember->chunk(2);
+        $setting    = Setting::first();
+
+        // dd($datamember);
+
         $no = 1;
-        $pdf = PDF::loadView('member.cetak', compact('datamember', 'no'));
-        $pdf->setPaper('a4', 'potrait');
+        $pdf = PDF::loadView('member.cetak', compact('datamember', 'no', 'setting'));
+        $pdf->setPaper(array(0, 0, 566.93, 850.39), 'potrait');
         return $pdf->stream('member.pdf');
     }
 }
